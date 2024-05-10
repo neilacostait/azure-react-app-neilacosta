0f18867854b1b32f8b6155c7aac7269adb31036a
Neil Acosta <neilacostait@gmail.com>
2024-05-10 13:06:36 -0700

    redeploy with new docker route image route

--- main.tf
+++ main.tf
@@ -1,49 +1,52 @@
-#resource "azurerm_resource_group" "react-app-resource" {
-#  name     = "react-app-resource"
-#  location = "East US"
-#}
-#
-#resource "azurerm_service_plan" "react-app-resource-service_plan_id" {
-#  name                = "react-app-resource-service_plan_id"
-#  resource_group_name = azurerm_resource_group.react-app-resource.name
-#  location            = azurerm_resource_group.react-app-resource.location
-#  os_type             = "Linux"
-#  sku_name            = "B1"
-#}
-#
-#resource "azurerm_linux_web_app" "web-react-app-neilacosta-tf" {
-#  name                = "web-react-app-neilacosta-tf"
-#  resource_group_name = azurerm_resource_group.react-app-resource.name
-#  location            = azurerm_service_plan.react-app-resource-service_plan_id.location
-#  service_plan_id     = azurerm_service_plan.react-app-resource-service_plan_id.id
-#
-#  app_settings = {
-#    DOCKER_REGISTRY_SERVER_URL      = var.DOCKER_REGISTRY_SERVER_URL
-#    DOCKER_REGISTRY_SERVER_USERNAME = var.DOCKER_REGISTRY_SERVER_USERNAME
-#    DOCKER_REGISTRY_SERVER_PASSWORD = var.DOCKER_REGISTRY_SERVER_PASSWORD
-#    WEBSITES_ENABLE_APP_SERVICE_STORAGE = "false"
-#    WEBSITES_PORT = "8080"
-#    DOCKER_CUSTOM_IMAGE_NAME    = "azurereactappneilacosta/neilacostareactweb:45"
-#  }
-#
-#  site_config {
-#    always_on        = true
-#    }
-#    }
-#
-##Pulling the variables from terraform cloud into terraform build process
-#variable "DOCKER_REGISTRY_SERVER_URL" {
-# description = "URL for Azure Docker Registry Server"
-#}
-#
-#variable "DOCKER_REGISTRY_SERVER_USERNAME" {
-#  description = "Username for Azure Docker Registry Server"
-#}
-#
-#variable "DOCKER_REGISTRY_SERVER_PASSWORD" {
-#  description = "Password for Azure Docker Registry Server"
-#}
-#
-#
-#
-#
+resource "azurerm_resource_group" "react-app-resource2" {
+  name     = "react-app-resource2"
+  location = "East US"
+}
+
+resource "azurerm_service_plan" "react-app-resource2-service_plan_id" {
+  name                = "react-app-resource2-service_plan_id"
+  resource_group_name = azurerm_resource_group.react-app-resource2.name
+  location            = azurerm_resource_group.react-app-resource2.location
+  os_type             = "Linux"
+  sku_name            = "B1"
+}
+
+resource "azurerm_linux_web_app" "web-react-app-neilacosta-tf" {
+  name                = "web-react-app-neilacosta-tf"
+  resource_group_name = azurerm_resource_group.react-app-resource2.name
+  location            = azurerm_service_plan.react-app-resource2-service_plan_id.location
+  service_plan_id     = azurerm_service_plan.react-app-resource2-service_plan_id.id
+
+  app_settings = {
+    DOCKER_REGISTRY_SERVER_URL      = var.DOCKER_REGISTRY_SERVER_URL
+    DOCKER_REGISTRY_SERVER_USERNAME = var.DOCKER_REGISTRY_SERVER_USERNAME
+    DOCKER_REGISTRY_SERVER_PASSWORD = var.DOCKER_REGISTRY_SERVER_PASSWORD
+    WEBSITES_ENABLE_APP_SERVICE_STORAGE = "false"
+    WEBSITES_PORT = "8080"
+    DOCKER_CUSTOM_IMAGE_NAME    = var.DOCKER_CUSTOM_IMAGE_NAME
+  }
+
+  site_config {
+    always_on        = true
+    }
+    }
+
+#Pulling the variables from terraform cloud into terraform build process
+variable "DOCKER_REGISTRY_SERVER_URL" {
+ description = "URL for Azure Docker Registry Server"
+}
+
+variable "DOCKER_REGISTRY_SERVER_USERNAME" {
+  description = "Username for Azure Docker Registry Server"
+}
+
+variable "DOCKER_REGISTRY_SERVER_PASSWORD" {
+  description = "Password for Azure Docker Registry Server"
+}
+
+variable "DOCKER_CUSTOM_IMAGE_NAME" {
+  description = "Custom image route name inside Azure Container Registry"
+}
+
+
+
