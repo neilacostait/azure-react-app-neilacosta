30706d2c891710a40797767a9b2aaddf54852023
Neil Acosta <neilacostait@gmail.com>
2024-05-10 20:48:27 -0700

    deployment tf - updated registry name

--- main.tf
+++ main.tf
@@ -3,9 +3,9 @@ resource "azurerm_resource_group" "react-app-resource2" {
   location = "West US"
 }
 
-resource "azurerm_container_registry" "azurereactappneilacosta-registry" {
+resource "azurerm_container_registry" "azurereactappneilacostaregistry" {
  
-  name                = "azurereactappneilacosta-registry"
+  name                = "azurereactappneilacostaregistry"
   resource_group_name = azurerm_resource_group.react-app-resource2.name
   location            = azurerm_resource_group.react-app-resource2.location
   sku                 = "Standard"
@@ -27,12 +27,12 @@ resource "azurerm_linux_web_app" "neilacosta-web-react-app-tf" {
   service_plan_id     = azurerm_service_plan.react-app-resource2-service_plan_id.id
   
   app_settings = {
-  #  DOCKER_REGISTRY_SERVER_URL          = azurerm_container_registry.azurereactappneilacosta-registry.login_server
-  #  DOCKER_REGISTRY_SERVER_USERNAME     = azurerm_container_registry.azurereactappneilacosta-registry.admin_username
-  #  DOCKER_REGISTRY_SERVER_PASSWORD     = azurerm_container_registry.azurereactappneilacosta-registry.admin_password
+  #  DOCKER_REGISTRY_SERVER_URL          = azurerm_container_registry.azurereactappneilacostaregistry.login_server
+  #  DOCKER_REGISTRY_SERVER_USERNAME     = azurerm_container_registry.azurereactappneilacostaregistry.admin_username
+  #  DOCKER_REGISTRY_SERVER_PASSWORD     = azurerm_container_registry.azurereactappneilacostaregistry.admin_password
     WEBSITES_ENABLE_APP_SERVICE_STORAGE = false
     WEBSITES_PORT                       = "80"
-  #  DOCKER_CUSTOM_IMAGE_NAME            = "${azurerm_container_registry.azurereactappneilacosta-registry.login_server}/neildockerimageappreact:latest"
+  #  DOCKER_CUSTOM_IMAGE_NAME            = "${azurerm_container_registry.azurereactappneilacostaregistry.login_server}/neildockerimageappreact:latest"
   }
   #app_settings = {
   #  DOCKER_REGISTRY_SERVER_URL      = var.DOCKER_REGISTRY_SERVER_URL
@@ -46,12 +46,12 @@ resource "azurerm_linux_web_app" "neilacosta-web-react-app-tf" {
   site_config {
     always_on = "true"
     application_stack {
-  docker_image_name        = "${azurerm_container_registry.azurereactappneilacosta-registry.name}/neildockerimageappreact:latest"
-  docker_registry_url      = "https://${azurerm_container_registry.azurereactappneilacosta-registry.login_server}"
-  docker_registry_username = azurerm_container_registry.azurereactappneilacosta-registry.admin_username
-  docker_registry_password = azurerm_container_registry.azurereactappneilacosta-registry.admin_password
+  docker_image_name        = "${azurerm_container_registry.azurereactappneilacostaregistry.name}/neildockerimageappreact:latest"
+  docker_registry_url      = "https://${azurerm_container_registry.azurereactappneilacostaregistry.login_server}"
+  docker_registry_username = azurerm_container_registry.azurereactappneilacostaregistry.admin_username
+  docker_registry_password = azurerm_container_registry.azurereactappneilacostaregistry.admin_password
 }
-  #linux_fx_version = "DOCKER|${azurerm_container_registry.azurereactappneilacosta-registry.login_server}/neildockerimageappreact:latest"
+  #linux_fx_version = "DOCKER|${azurerm_container_registry.azurereactappneilacostaregistry.login_server}/neildockerimageappreact:latest"
   }
 }
 #Pulling the variables from terraform cloud into terraform build process
